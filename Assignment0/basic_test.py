e3524d6acc4d48700f8bde120a5db5c1906b538b
Marek Rychlik <rychlik@arizona.edu>
2024-07-30 10:05:46 -0700

    *** empty log message ***

--- Assignment0/basic_test.py
+++ Assignment0/basic_test.py
@@ -15,6 +15,6 @@ import solve_quadratic_equation as quadratic
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         roots = quadratic.solve_quadratic_equation(1, -1000000.001, 1)
-        print("Testing with unittest, indeed...")
+        #print("Testing with unittest, indeed...")
         self.assertAlmostEqual(roots[0], 1000000, places=7, msg = "Not enough places")
         self.assertAlmostEqual(roots[0], 1000000, delta=1e-7, msg = "Delta not met")
